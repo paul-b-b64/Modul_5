55f627641c7446f651099d6c1681586f0a69686c
paul-b-b64 <belousko@inbox.ru>
2024-07-17 12:40:05 +0300

    ДЗ 5.1

---
+++ Modul_5_1.py
@@ -0,0 +1,15 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+Dom1 = House('ЖК Мирный', 14)
+Dom1.go_to(7)
