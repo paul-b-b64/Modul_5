c9b9055ee41208db61c50043ea5cb26cbc8e2b2e
paul-b-b64 <belousko@inbox.ru>
2024-07-27 18:51:50 +0300

    все копируем

--- Modul5hard.py
+++ Modul5hard.py
@@ -1,45 +1,40 @@
 class UrTube:
-    users = {}
-    videos = []
-    current_user = None
-
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = []
 
     def register(self, nickname, password, age):
-        nickname = input('Введите логин: ')
-        password = hash(input('Введите пароль: '))
-        age = input('Ваш возраст: ')
-        if nickname in UrTube.users:
+        hpass = hash(password)
+        if nickname in self.users:
             print(f'Пользователь {nickname} уже существует')
         else:
-            UrTube.current_user = User(nickname, password, age)
+            self.current_user = User(nickname, hpass, age)
+            self.users.append(self.current_user)
 
-
-    # def log_in(nickname, password):
-    #     nickname = input('Введите логин: ')
-    #     password = input('Введите пароль: ')
-    #
-    #     if nickname in users:
-    #         if password == users[]:
-    #             print(f'Вход выполнен, {login}')
-    #             break
-    #         else:
-    #             print('Неверный пароль')
-    #     else:
-    #         print('Пользователь не найден')
-    #
-
-
+    def log_in(self, nickname, password):
+        for i in
+        if nickname in self.users:
+            if hash(password) == self.users[nickname]:
+                print(f'Вход выполнен, {nickname}')
+                self.current_user = self.users[nickname]
+            else:
+                print('Неверный пароль')
+        else:
+            print('Нет такого пользователя, зарегистрируйтесь!')
+            # self.register()
 class Video:
-    time_now = 0
-    adult_mode = False
 
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
+        time_now = 0
+        adult_mode = False
 
     def __str__(self):
         return (f'Название: {self.title}, Продолжительность: {self.duration},'
                 f' Остановлен на: {self.time_now}, {self.adult_mode}')
+
 
 # vid = Video('Gustav', 2)
 # vid.time_now = 6
@@ -48,14 +43,53 @@ class Video:
 class User:
     def __init__(self, nickname, password, age):
         self.nickname = nickname
-        self.password = password
+        self.password = hash(password)
         self.age = age
 
-# User1 = User('glj', 'faf', 24)
-# print(User1.password)
-# print(hash('faf'))
+if __name__ == '__main__':
+    ur = UrTube()
+    #
+    # while True:
+        # choice = int(input('Здравствуйте! Выберите действие: \n1 - Вход\n2 - Регистрация\n'))
+        # if choice == 1:
+        #     ur.register('nickname', 'password', 22)
+        #     if ur.current_user in ur.users:
+        #         print(f'Вход выполнен, {ur.current_user.nickname}')
+        #     #         break
+        #     #     else:
+        #     #         print('Неверный пароль')
+        #     # else:
+        #     #     print('Пользователь не найден')
+        # # if choice == 2:
+        # #     user = User(input('Введите логин:'), password := input('Введите пароль:'),
+        # #                 password2 := input('Подтвердите пароль:'))
+        # #     # if len(password) < 4:
+        # #     #     print('короткий пароль')
+        # #     if password != password2:
+        # #         print('Пароли не совпадают, пробуйте еще')
+        # #         continue
+        # #     database.add_user(user.username, user.password)
+        # # print(database.data)
+        #
+    user1 = User('Семен', 'dgld', 25)
+    user2 = User('Герхардт', 'gdcle', age=34)
+    ur.current_user = [user1.nickname, user1.password, user1.age]
+    ur.users.append(ur.current_user)
+    ur.current_user = [user2.nickname, user2.password, user2.age]
+    print(ur.current_user)
+    ur.users.append(ur.current_user)
+    print(ur.users)
+    print(ur.current_user in ur.users)
+    print(ur.users[0])
+    print('Семен' in ur.users[0])
+    print('Семен' in ur.users[1])
+    for f in ur.users:
+        print('Семен' in f)
+    #
 
 
-ur = UrTube()
-ur.register('nickname', 'password', 22)
-print(ur.current_user)
+
+
+    # ur.log_in('Семен', 'dgld')
+    # print(ur.current_user)
+
